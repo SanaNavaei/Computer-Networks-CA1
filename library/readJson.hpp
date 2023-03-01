fa4c2a5a5b6caf93e75374c85cc7a7649f535a99
SanaNavaei <sana.sarinavaei@gmail.com>
2023-03-02 02:18:33 +0330

    Add files via upload

---
+++ library/readJson.hpp
@@ -0,0 +1,26 @@
+#ifndef READJSON_HPP_
+#define READJSON_HPP_ "READJSON_HPP_"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "json.hpp"
+#include "user.hpp"
+#include "admin.hpp"
+#include "rooms.hpp"
+
+using json = nlohmann::json;
+class readJson
+{
+public:
+    readJson();
+private:
+    std::string hostName;
+    std::vector<User*> users;
+    std::vector<Admin*> admins;
+    std::vector<Rooms*> rooms;
+    int port;
+};
+
+#endif

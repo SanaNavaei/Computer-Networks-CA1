0f954c7d67dfb28ec899e7e4317e1de36b3ffc92
SanaNavaei <sana.sarinavaei@gmail.com>
2023-03-03 02:34:32 +0330

    Add some functions

--- library/user.hpp
+++ library/user.hpp
@@ -7,6 +7,8 @@ class User
 {
 public:
     User(int id_, std::string user_, std::string password_, std::string purse_, std::string phoneNumber_, std::string address_);
+    std::string getname();
+    std::string getpassword();
 private:
     int id;
     std::string user;

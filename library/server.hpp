532d25c681e5df95fd26dce4c1f2ba78c66bddbe
SanaNavaei <sana.sarinavaei@gmail.com>
2023-03-03 21:12:05 +0330

    define some errors

--- library/server.hpp
+++ library/server.hpp
@@ -6,17 +6,30 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
+#include <sstream>
 #include <unistd.h>
 #include "readJson.hpp"
 
 #define MAX_CLIENTS 10
+#define ERR230 "230: User logged in."
+#define ERR231 "231: User successfully signed up."
+#define ERR311 "311: User Signed up. Enter your password, purse, phone and address."
+#define ERR401 "401: Invalid value!"
+#define ERR430 "430: Invalid username or password."
+#define ERR451 "451: User already existed!"
+#define ERR503 "503: Bad sequence of commands."
 
 class Server
 {
 public:
     Server(readJson data);
     void build();
+    void signup(std::string username, std::string password, std::string purse, std::string phoneNumber, std::string address, int fd);
+    void checkCommand(char buff[], int fd);
+    void checkusername(std::string name, int fd);
+    void signin(std::string username, std::string password, int fd);
     int setup_server(int port);
+    bool checkIsANumber(std::string input, int fd);
 private:
     readJson data;
 };

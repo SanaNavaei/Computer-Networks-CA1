95c55a2665678efa0165efbbfdab4d4ac5a7a680
SanaNavaei <sana.sarinavaei@gmail.com>
2023-03-06 19:29:18 +0330

    Update user.hpp

--- library/user.hpp
+++ library/user.hpp
@@ -9,6 +9,7 @@ public:
     User(int id_, std::string user_, std::string password_, std::string purse_, std::string phoneNumber_, std::string address_);
     std::string getname();
     std::string getpassword();
+    int getid();
 private:
     int id;
     std::string user;

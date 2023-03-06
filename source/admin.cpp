7ac0f2f989818079046fdf51d176520605a5df5a
SanaNavaei <sana.sarinavaei@gmail.com>
2023-03-06 19:29:50 +0330

    add some functions

--- source/admin.cpp
+++ source/admin.cpp
@@ -6,3 +6,18 @@ Admin::Admin(int id_, std::string user_, std::string password_)
     user = user_;
     password = password_;
 }
+
+std::string Admin::getname()
+{
+    return user;
+}
+
+std::string Admin::getpassword()
+{
+    return password;
+}
+
+int Admin::getid()
+{
+    return id;
+}

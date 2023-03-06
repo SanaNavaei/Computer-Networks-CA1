3cf3e3362500330f3b93e624fd8cfc1db6c7adfa
SanaNavaei <sana.sarinavaei@gmail.com>
2023-03-06 23:39:25 +0330

    add some functions

--- library/user.hpp
+++ library/user.hpp
@@ -9,6 +9,11 @@ public:
     User(int id_, std::string user_, std::string password_, std::string purse_, std::string phoneNumber_, std::string address_);
     std::string getname();
     std::string getpassword();
+    std::string getphone();
+    std::string getaddress();
+    void setpassword(std::string password_);
+    void setaddress(std::string address_);
+    void setphoneNumber(std::string phoneNumber_);
     int getid();
 private:
     int id;

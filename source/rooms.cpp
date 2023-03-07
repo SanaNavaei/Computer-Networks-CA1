a88ee6e2190b29471b187cd785cb3a5e20e0be31
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-03-07 15:10:25 +0330

    add some methods for getter.

--- source/rooms.cpp
+++ source/rooms.cpp
@@ -9,3 +9,34 @@ Rooms::Rooms(std::string number_, int status_, int price_, int maxCapacity_, int
     capacity = capacity_;
     userInRooms = userInRooms_;
 }
+
+std::string Rooms::getnum()
+{
+    return number;
+}
+
+int Rooms::getstatus()
+{
+    return status;
+}
+
+int Rooms::getprice()
+{
+    return price;
+}
+
+int Rooms::getmax_capacity()
+{
+    return maxCapacity;
+}
+
+int Rooms::getcapacity()
+{
+    return capacity;
+}
+
+std::vector<userInRoom> Rooms::getusers()
+{
+    return userInRooms;
+}
+

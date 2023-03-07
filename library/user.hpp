1709b5058d1863112c7c11df7590e4f6a6f1866b
SanaNavaei <sana.sarinavaei@gmail.com>
2023-03-07 21:50:41 +0330

    clean the code

--- library/user.hpp
+++ library/user.hpp
@@ -7,14 +7,17 @@ class User
 {
 public:
     User(int id_, std::string user_, std::string password_, std::string purse_, std::string phoneNumber_, std::string address_);
+    
     std::string getname();
     std::string getpassword();
     std::string getphone();
     std::string getaddress();
     std::string getpurse();
+    
     void setpassword(std::string password_);
     void setaddress(std::string address_);
     void setphoneNumber(std::string phoneNumber_);
+    
     int getid();
 private:
     int id;

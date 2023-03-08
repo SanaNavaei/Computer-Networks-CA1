f51af4a4fd80b0513aa0cec30c3b559819060e79
SanaNavaei <sana.sarinavaei@gmail.com>
2023-03-09 01:39:38 +0330

    add some functions to write in json files

--- source/readJson.cpp
+++ source/readJson.cpp
@@ -22,6 +22,67 @@ void readJson::write_signedup(std::string data)
     file.close();
 }
 
+//write edit info of user in Userinfo.json
+void readJson::write_editinfo(std::string data)
+{
+    std::ifstream user_file("./UserInfo.json");
+
+    json user_data = json::parse(user_file);
+    user_file.close();
+
+    for (auto& user : user_data["users"])
+    {
+        if (user["id"] == json::parse(data)["id"])
+        {
+            if (user["admin"] == "true")
+            {
+                user["password"] = json::parse(data)["password"];
+                continue;
+            }
+            user["password"] = json::parse(data)["password"];
+            user["phoneNumber"] = json::parse(data)["phoneNumber"];
+            user["address"] = json::parse(data)["address"];
+        }
+    }
+    std::ofstream file("./UserInfo.json");
+    file << std::setw(4) << user_data;
+    file.close();
+}
+
+//remove user from room in RoomInfo.json
+void readJson::write_leaveroom(std::string data)
+{
+    std::ifstream room_file("./RoomsInfo.json");
+
+    json room_data = json::parse(room_file);
+    room_file.close();
+
+    for (auto& room : room_data["rooms"])
+    {
+        json newarray = json::array();
+        if (room["number"] == json::parse(data)["number"])
+        {
+            for (auto& user : room["users"])
+            {
+                if (user["id"] == json::parse(data)["id"])
+                {
+                    //set new capacity of room
+                    room["capacity"] = json::parse(data)["capacity"];
+                    room["status"] = json::parse(data)["status"];
+                    user.clear();
+                }
+                else
+                    newarray.emplace_back(user);
+            }
+            room["users"] = newarray;
+        }
+    }
+    
+    std::ofstream file("./RoomsInfo.json");
+    file << std::setw(4) << room_data;
+    file.close();
+}
+
 readJson::readJson()
 {
     //Read config.json

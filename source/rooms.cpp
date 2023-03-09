0ff1f053ac21e90c28be34afd1ba65362efa3748
SanaNavaei <sana.sarinavaei@gmail.com>
2023-03-09 10:48:10 +0330

    Update rooms.cpp

--- source/rooms.cpp
+++ source/rooms.cpp
@@ -50,68 +50,6 @@ void Rooms::set_maxcap(int maxCapacity_)
     maxCapacity = maxCapacity_;
 }
 
-#include "../library/rooms.hpp"
-
-Rooms::Rooms(std::string number_, int status_, int price_, int maxCapacity_, int capacity_, std::vector<userInRoom> userInRooms_)
-{
-    number = number_;
-    status = status_;
-    price = price_;
-    maxCapacity = maxCapacity_;
-    capacity = capacity_;
-    userInRooms = userInRooms_;
-}
-
-std::string Rooms::getnum()
-{
-    return number;
-}
-
-int Rooms::getstatus()
-{
-    return status;
-}
-
-int Rooms::getprice()
-{
-    return price;
-}
-
-int Rooms::getmax_capacity()
-{
-    return maxCapacity;
-}
-
-int Rooms::getcapacity()
-{
-    return capacity;
-}
-
-std::vector<userInRoom> Rooms::getusers()
-{
-    return userInRooms;
-}
-
-void Rooms::set_price(int price_)
-{
-    price = price_;
-}
-
-void Rooms::set_maxcap(int maxCapacity_)
-{
-    maxCapacity = maxCapacity_;
-}
-
-void Rooms::set_capacity(int capacity_)
-{
-    capacity = capacity_;
-}
-
-void Rooms::set_userInRooms(std::vector<userInRoom> userInRooms_)
-{
-    userInRooms = userInRooms_;
-}
-
 void Rooms::set_capacity(int capacity_)
 {
     capacity = capacity_;

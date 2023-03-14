e3b93f31afaedd727baec007faa6cb0a5a2fa58f
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-03-14 12:57:16 +0330

    add some libraries for log part

--- library/client.hpp
+++ library/client.hpp
@@ -8,9 +8,13 @@
 #include <fcntl.h>
 #include <sstream>
 #include <unistd.h>
+#include <chrono>
+#include <ctime>
+#include <sys/stat.h>//for mkdir
 #include "readJson.hpp"
 
 #define ERR230 "230: User logged in."
+#define ERR231 "231: User successfully signed up."
 #define ERR311 "311: User Signed up. Enter your password, purse, phone and address."
 #define ERR403 "403: Access denied!"
 #define ERR503 "503: Bad sequence of commands."

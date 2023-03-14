ca16a01cc47ef95c16fbaf0341734b05bee8355e
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-03-14 08:43:55 -0700

    add error.hpp header file

---
+++ library/error.hpp
@@ -0,0 +1,26 @@
+#ifndef ERROR_HPP
+#define ERROR_HPP "ERROR_HPP"
+
+#define ERR101 "101: The desired room was not found."
+#define ERR102 "102: Your reservation was not found."
+#define ERR104 "104: Successfully added."
+#define ERR105 "105: Successfully modified."
+#define ERR106 "106: Successfully deleted."
+#define ERR108 "108: Your account balance is not enough."
+#define ERR109 "109: The room capacity is full."
+#define ERR110 "110: Successfully done."
+#define ERR111 "111: This room already exists."
+#define ERR201 "201: User logged out successfully."
+#define ERR230 "230: User logged in."
+#define ERR231 "231: User successfully signed up."
+#define ERR311 "311: User Signed up. Enter your password, purse, phone and address."
+#define ERR312 "312: Information was changed successfully."
+#define ERR401 "401: Invalid value!"
+#define ERR403 "403: Access denied!"
+#define ERR412 "412: Invalid capacity value!"
+#define ERR413 "413: Successfully Leaving."
+#define ERR430 "430: Invalid username or password."
+#define ERR451 "451: User already existed!"
+#define ERR503 "503: Bad sequence of commands."
+
+#endif
